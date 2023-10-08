66f43ae9304a6583c5ddb90f6baf2b3487898e8d
knuch <knuchelf@gmail.com>
2023-10-08 14:46:46 +0200

    Updates

--- src/data/templates.js
+++
@@ -1,3 +0,0 @@
-import postfinance_debit_ynab from "./templates/postfinance_debit_ynab";
-import postfinance_credit_ynab from "./templates/postfinance_credit_ynab";
-export default [postfinance_debit_ynab, postfinance_credit_ynab];
